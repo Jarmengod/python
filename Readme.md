0c8ad3337ca2193c68433c1357bf59a5697f58a9
Armengod <josep.armengod@hp.com>
2017-07-12 18:17:24 +0200

    python 1st commit; raining git gui Signed-off-by: Armengod <josep.armengod@hp.com>

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Repositorio para aprender python
+Diferentes programas para aprender python basado en el libro  
+Beginning Programming With Python For Dummies - 2014
+
+Instalo spyder (winpython)
---
+++ cap1_examples.py
@@ -0,0 +1,21 @@
+"""
+   ejemplo de comentarios de varias lineas
+   Program to learn python
+    based in ebook begining programming with python for dummies 2014
+
+   Filename:    cap1_examples.py
+   Autor:       Josep Armengod
+   Date:        11 Jul 2017
+   Purpose:     Did examples of cap 1 book to learn python
+   
+   Revision history:
+    v1.0:   Start
+    
+"""
+
+# Ejemplo de coment en una linea
+
+print("Hello world " +
+"Good bye moon")      # comment detras de linea
+
+print("codigo despues del comentario")
